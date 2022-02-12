a6f66aa72ef0d8f2075dc0750ae26ac546dfdfab
Kelly Soares <kellyps_2012@yahoo.com.br>
2022-02-12 12:02:02 -0300

    Criacao e organização das pastas e banco de dados

---
+++ api_vendas_trinto.sql
@@ -0,0 +1,59 @@
+
+
+-- Copiando estrutura do banco de dados para api_vendas_trinto
+DROP DATABASE IF EXISTS `api_vendas_trinto`;
+CREATE DATABASE IF NOT EXISTS `api_vendas_trinto` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `api_vendas_trinto`;
+
+-- Copiando estrutura para tabela api_vendas_trinto.produtos
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto` varchar(150) NOT NULL,
+  `preco` float NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela api_vendas_trinto.vendas
+DROP TABLE IF EXISTS `vendas`;
+CREATE TABLE IF NOT EXISTS `vendas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_vendedor` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL DEFAULT '0',
+  `preco_unit` float NOT NULL DEFAULT '0',
+  `preco_total` float NOT NULL DEFAULT '0',
+  `data` date NOT NULL,
+  `hora` time NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `FK1_produto_vends` (`id_produto`),
+  KEY `FK2_vendedor_vendas` (`id_vendedor`),
+  CONSTRAINT `FK1_produto_vends` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `FK2_vendedor_vendas` FOREIGN KEY (`id_vendedor`) REFERENCES `vendedores` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela api_vendas_trinto.vendedores
+DROP TABLE IF EXISTS `vendedores`;
+CREATE TABLE IF NOT EXISTS `vendedores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `senha` varchar(150) NOT NULL,
+  `salt` varchar(150) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf` (`cpf`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
